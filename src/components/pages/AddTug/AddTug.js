347f54df2f88435c21371d22fc3cb7f04fc8de52
Maggie Leavell <mjleavell@gmail.com>
2019-01-22 18:31:57 -0600

    wip

--- src/components/pages/AddTug/AddTug.js
+++ src/components/pages/AddTug/AddTug.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Col,
+} from 'reactstrap';
 
 import './AddTug.scss';
 
@@ -7,6 +15,45 @@ class AddTug extends React.Component {
     return (
       <div className="AddTug">
         <h3>Add Tug to Fleet</h3>
+        <Form>
+          <FormGroup row>
+            <Label for="tug-name">Tug Name</Label>
+            <Col sm={10}>
+              <Input type="text" id="tug-name" placeholder="Enter name of tug" />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="tug-mmsi">Tug MMSI</Label>
+            <Col sm={10}>
+              <Input type="text" id="tug-mmsi" placeholder="Enter mmsi" />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="tug-homeport">Home Port</Label>
+            <Col sm={10}>
+              <Input type="text" id="tug-homeport" placeholder="Enter home port" />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="tug-homeport-lat">Latitude</Label>
+            <Col sm={10}>
+              <Input type="text" id="tug-homeport-lat" placeholder="Enter home port latitude" />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="tug-homeport-lon">Longitude</Label>
+            <Col sm={10}>
+              <Input type="text" id="tug-homeport-lon" placeholder="Enter home port longitude" />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="tug-Captain">Captain</Label>
+            <Col sm={10}>
+              <Input type="text" id="tug-captain" placeholder="Enter captain" />
+            </Col>
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
       </div>
     );
   }
