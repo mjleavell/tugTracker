511a7ec708aa346c161031b41aae544f82e43d55
Maggie Leavell <mjleavell@gmail.com>
2019-01-25 11:47:50 -0600

    map is displayed

--- src/components/pages/Locations/Locations.js
+++ src/components/pages/Locations/Locations.js
@@ -1,5 +1,10 @@
 import React from 'react';
-
+import {
+  Map as LeafletMap,
+  Marker,
+  Popup,
+  TileLayer,
+} from 'react-leaflet';
 import './Locations.scss';
 
 class Locations extends React.Component {
@@ -7,6 +12,27 @@ class Locations extends React.Component {
     return (
       <div className="Locations">
         <h3>All tugs will be displayed on map</h3>
+        <LeafletMap
+          center={[50, 10]}
+          zoom={6}
+          maxZoom={10}
+          attributionControl={true}
+          zoomControl={true}
+          doubleClickZoom={true}
+          scrollWheelZoom={true}
+          dragging={true}
+          animate={true}
+          easeLinearity={0.35}
+        >
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={[50, 10]}>
+            <Popup>
+              Popup for any custom information.
+            </Popup>
+          </Marker>
+        </LeafletMap>
       </div>
     );
   }
