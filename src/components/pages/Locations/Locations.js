8f7462e73e8779a9b6119e633677af2ababf0153
Maggie Leavell <mjleavell@gmail.com>
2019-01-29 14:27:19 -0600

    wip

--- src/components/pages/Locations/Locations.js
+++ src/components/pages/Locations/Locations.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Map from '../../Map/Map';
 import tugRequests from '../../../helpers/data/tugRequests';
 import authRequests from '../../../helpers/data/authRequests';
+import marineTrafficRequests from '../../../helpers/data/marineTrafficRequests';
 import './Locations.scss';
 
 class Locations extends React.Component {
@@ -11,10 +12,36 @@ class Locations extends React.Component {
 
   getAllTugs() {
     const uid = authRequests.getCurrentUid();
-    tugRequests.getTugs(uid)
-      .then((tugs) => {
-        this.setState({ tugs });
-      })
+    tugRequests.getTugs(uid).then((tugs) => {
+      
+      tugs.forEach((singleTug) => {
+        marineTrafficRequests.getTugExtended(singleTug.mmsi).then((result) => {
+          this.setState({ tugs });
+          const tugInfo = result;
+          if (tugInfo.length === 0) {
+            this.setState({ selectedTug: [singleTug] });
+          } else {
+            tugInfo.forEach((item) => {
+              this.setState({
+                selectedTug: [{
+                  id: singleTug.id,
+                  name: singleTug.name,
+                  mmsi: singleTug.mmsi,
+                  captain: singleTug.captain,
+                  currentLat: parseFloat(item[1]),
+                  currentLon: parseFloat(item[2]),
+                  speed: parseFloat(item[3]),
+                  lastPort: item[20],
+                  nextPort: item[31],
+                  inEdit: singleTug.inEdit,
+                  uid: singleTug.uid,
+                }],
+              });
+            });
+          }
+        });
+      });
+    })
       .catch(err => console.error('error in getAllTugs', err));
   }
 
